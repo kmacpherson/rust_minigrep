ad97c24614abaab6c6098d545905230429d2b4a4
Kevin MacPherson <kevin@teamroke.ca>
2019-08-04 12:37:33 -0400

    minor refactor

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(( | err | {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    }));
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("File Not found");
+    run(config);
+}
+
+fn run(config: Config) {
+    let mut f = File::open(config.filename).expect("File Not found");
 
     let mut contents = String::new();
 
     f.read_to_string(&mut contents).expect("Something went wrong");
 
     println!("With text:\n{}", contents);
+}
+
+struct  Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
