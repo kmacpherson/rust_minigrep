484e464bd8106551aab069bbb277163ecc4ea477
Kevin MacPherson <kevin@teamroke.ca>
2019-08-05 19:25:25 -0400

    Moving parts to lib file.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,35 +13,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    run(config);
-}
-
-fn run(config: Config) {
-    let mut f = File::open(config.filename).expect("File Not found");
-
-    let mut contents = String::new();
-
-    f.read_to_string(&mut contents).expect("Something went wrong");
-
-    println!("With text:\n{}", contents);
-}
-
-struct  Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
